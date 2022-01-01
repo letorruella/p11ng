6869787b10e974649d0ce3679ddcd224c093e7d1
Luis Torruella <luis.torruella@grandrounds.com>
2022-01-01 13:34:43 -0500

    added django-heroku to locals

--- core/settings.py
+++ core/settings.py
@@ -6,6 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 import os
 from decouple import config
 from unipath import Path
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).parent
@@ -123,6 +124,7 @@ STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
 )
 
+django_heroku.settings(locals())
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
