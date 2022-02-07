e47b385c711f77c985daa117b577d34c0061b8fc
Luis Torruella <luis.torruella@grandrounds.com>
2022-02-07 10:10:24 -0500

    first commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "p11ng": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
